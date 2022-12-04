14efa476bb736a454ec36f67014a5aa96458bb88
Aisyazhs <aisyahazizahs2000@gmail.com>
2022-12-05 00:54:27 +0700

    first commit

---
+++ src/test/java/ws/a/contoh/ContohApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.contoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
